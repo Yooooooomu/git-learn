d891e847a976580cd4de070083f3ef21e178338d
Yooooooomu <49017287+Yooooooomu@users.noreply.github.com>
2020-09-02 16:29:08 +0800

    install devServer
    安装dev-server等模块

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log('main.js');
+console.log(process.env.NODE_ENV);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'git learn',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
